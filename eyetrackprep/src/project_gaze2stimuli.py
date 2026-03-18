c536bf315b042dcbcf1006c76f44da7652530957
MarieStLaurent <marie.stl@gmail.com>
2026-03-18 19:20:02 -0400

    wip gze2frame emotions

--- eyetrackprep/src/project_gaze2stimuli.py
+++ eyetrackprep/src/project_gaze2stimuli.py
@@ -154,7 +154,7 @@ def drawgaze(clip,fx,fy,r_zone):
         region_size = y2-y1,x2-x1
 
         orig = im[y1:y2, x1:x2]
-        circled = cv2.circle(orig, (r_zone, r_zone), r_zone, (155, 0, 155), -1,
+        circled = cv2.circle(orig, (r_zone, r_zone), r_zone, (255, 0, 0), -1,
                              lineType=cv2.LINE_AA)
 
         im[y1:y2, x1:x2] = circled
