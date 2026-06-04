842a82dda3c0cc67f9d290a44ce12f1eb0f2fad5
MarieStLaurent <marie.stl@gmail.com>
2026-06-04 18:46:43 -0400

    framework clean filenames

---
+++ eyetrackprep/scripts/cleanup_scripts/clean_emotionvideos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BIDS_DIR="/data/neuromod/projects/eyetracking_bids/bids_repos/emotion-videos"
+DERIV_DIR="/data/neuromod/projects/eyetracking_bids/deriv_repos/emotion-videos.eyetrackprep"
+
+# TODO: implement steps to rename the output files (figures, .json, .tsv.gz) 
+# to remove timestamps, reset run numbers and change the task name in the final files
