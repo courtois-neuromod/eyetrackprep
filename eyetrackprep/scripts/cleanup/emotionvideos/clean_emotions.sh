27eb5a311051bc2a69c65b08f97de12586fcd8af
MarieStLaurent <marie.stl@gmail.com>
2026-06-08 20:06:09 -0400

    cleanup emotion scripts

--- eyetrackprep/scripts/cleanup/emotionvideos/clean_emotions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BIDS_DIR="/data/neuromod/projects/eyetracking_bids/bids_repos/emotion-videos"
-DERIV_DIR="/data/neuromod/projects/eyetracking_bids/deriv_repos/emotion-videos.eyetrackprep"
-FILE_PATH="${DERIV_DIR}/code/eyetrackprep/eyetrackprep/scripts/cleanup_scripts/emotionsvideos_relabelruns.tsv"
-
-python clean_emotionvideos.py "${BIDS_DIR}" "${DERIV_DIR}" "${FILE_PATH}" 
